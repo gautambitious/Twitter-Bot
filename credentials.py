8da089734b21e92a302bc9eb4ab9a9fea0b8085c
gautambitious <mvgt.gautam@gmail.com>
2020-09-18 14:02:58 +0530

    Creds

--- credentials.py
+++
@@ -1,6 +0,0 @@
-class Creds:
-    def __init__(self):
-        self.consumer_key = 'Yr5Vf8dNGHiEYIoXjjp9pmGTa'
-        self.consumer_secret = 'Z01SseUCHTNwwMd593eCe2nedJOE9rdW8pHmkh4ilGKxquuwgX'
-        self.access_key = '594643331-dG8lGbD1Arb0d1bcyNVh8HcU4OTNium00fljr1ll'
-        self.access_secret = 'rURU5iPsD0DEbkY0bd7UIMneVWP1ZVczqtkZqpxa52tBr'
